77a9ea1b786cbed13e60868dd5d9d2184c965c20
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-01-20 17:05:37 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 04', () => {
+  test('Verifica se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/paginadesconhecida');
+    });
+
+    const h2 = screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('Verifica se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/paginadesconhecida');
+    });
+
+    const image = screen.getByRole('img');
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
